1f5df9cdafd55f6c724042076e01d8b81b88d589
Michael S. McDargh, Sr <mike@mcdargh.com>
2017-07-06 00:42:08 -0400

    Implemented OTA update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <Wire.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 
 /* Place your settings in the settings.h file for ssid, password, and dweet post string
 FILE: settings.h
@@ -8,6 +11,8 @@ FILE: settings.h
 const char* ssid = "MYSSID";
 const char* password = "MyPassword";
 const char* dweet = "/dweet/for/myservice?what=";
+const char* otapwd = "supersecret";
+NOTE:  Be sure to set this password also in the auth flag in platform.io
 */
 #include "settings.h"
 
@@ -53,7 +58,7 @@ struct StateMachine_t{
 
 // forwards
 void connectToSerial(void);
-void connectToWifi(void);
+bool connectToWifi(void);
 void sendToDweet(const char* log);
 void setSM(StateMachine_t& sm, float tpd, WinderDirection_t direction,
     uint8_t pwm, uint8_t pina, uint8_t pinb, const char* name);
@@ -61,18 +66,96 @@ void checkSM(StateMachine_t& sm);
 void startWinding(StateMachine_t& sm);
 void stopWinding(StateMachine_t& sm);
 void switchDirection(StateMachine_t& sm);
+void setupOTA();
 
 StateMachine_t RolexSM;
 StateMachine_t OmegaSM;
+bool wifiConnected = false;
 
 void setup()
 {
   connectToSerial();
-  connectToWifi();
+  if(connectToWifi()) {
+    setupOTA();
+  }
 
   //Setup outputs
   setSM(RolexSM, RolexTPD, All, RolexPWM, RolexPINA, RolexPINB, RolexNAME);
   setSM(OmegaSM, OmegaTPD, ClockWise, OmegaPWM, OmegaPINA, OmegaPINB, OmegaNAME);
+}
+
+void connectToSerial() {
+  Serial.begin(115200);
+  while(!Serial); //wait for serial port to connect (needed for Leonardo only)
+  Serial.println("Booting...");
+}
+
+bool connectToWifi() {
+  int numTries = 10;
+  // Connect to WiFi
+   WiFi.mode(WIFI_STA);
+   WiFi.begin(ssid, password);
+   while ((WiFi.status() != WL_CONNECTED) && (numTries-- > 0)) {
+     delay(500);
+     Serial.print(".");
+   }
+   if(WiFi.status() != WL_CONNECTED) {
+     Serial.println("WIFI not connected");
+     return false;
+   }
+
+   wifiConnected = true;
+   Serial.println("");
+   Serial.println("WiFi connected");
+
+   // Print the IP address
+   Serial.println(WiFi.localIP());
+   return true;
+}
+
+
+void setupOTA() {
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  ArduinoOTA.setHostname("watchwinder");
+
+  // No authentication by default
+  ArduinoOTA.setPassword(otapwd);
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  
 }
 
 void setSM(StateMachine_t& sm, float tpd, WinderDirection_t direction,
@@ -112,30 +195,13 @@ void setSM(StateMachine_t& sm, float tpd, WinderDirection_t direction,
   analogWriteFreq(PWMFREQ);
 }
 
-
-void connectToSerial() {
-  Serial.begin(9600);
-  while(!Serial); //wait for serial port to connect (needed for Leonardo only)
-}
-
-void connectToWifi() {
-  // Connect to WiFi
-   WiFi.begin(ssid, password);
-   while (WiFi.status() != WL_CONNECTED) {
-     delay(500);
-     Serial.print(".");
-   }
-   Serial.println("");
-   Serial.println("WiFi connected");
-
-   // Print the IP address
-   Serial.println(WiFi.localIP());
-}
-
-
 void loop()
 {
   char log[256];
+
+  if(wifiConnected) {
+    ArduinoOTA.handle();
+  }
 
   Serial.println("In loop");
 
